998c7b5b838e59022685359540daa7ad63a3da27
sivavinoth <jmsivavinoth@example.com>
2024-08-10 11:14:44 +0530

    pusing basic folder

---
+++ basic/tt.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class tt {
+   public static void main(String[] args) {
+    for (int j = 1 ; j <= 3 ;j=j+1){
+        for (int i = 1 ; i <= j ;i=i+1){
+
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+ 
+}  
+}
